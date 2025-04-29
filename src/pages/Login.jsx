435dd7310dd9c32145de3e1e8349ca2acc2c9856
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-04-29 12:21:01 +0100

    feat: signup and login page styled basically

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,24 +3,31 @@ import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div className='min-h-screen flex justify-center items-center'>
+    <div className='min-h-screen flex justify-center items-center border-2 pt-10 '>
       <div className=''>
-        <h2 className='text-3xl text-bold'>Welcome Back!</h2>
-        <form action="">
-          <div>
-            <label htmlFor="" className='text-bold'>Email</label>
-            <input type="text" />
-          </div>
-          <div>
-            <label htmlFor="">Password</label>
-            <input type="text" />
-          </div>
+        <div>
+          <h2 className='text-3xl text-bold text-center text-gray-600'>Welcome Back!</h2>
+        </div>
+        <div>
+          <form action=""className='mb-20 mt-10 gap-4 flex flex-col'>
+            <div>
+              <input type="text" placeholder='Email' className='border-3 px-[0.5rem] py-[0.5rem] w-[21.42rem] rounded-[0.3rem] border-gray-300 '/>
+            </div>
+            <div>
+              <input type="text" placeholder='Password' className='border-3 px-[0.5rem] py-[0.5rem] w-[21.42rem] rounded-[0.3rem] border-gray-300 items-start'/>
+            </div>
 
-          <button>
-            Login
-          </button>
-          <p>Don't have an account yet <Link to="/register">Sign Up</Link></p>
-        </form>
+            <button className='border-2 px-[8rem] py-[0.5rem] rounded-[0.3rem] border-gray-400 bg-gray-500 items-center text-white textbold text-2xl'>
+              Login
+            </button>
+            <div>
+              <p className='text-center text-gray-600'>Forgot password?</p>
+            </div>
+            <div>
+              <p className='text-center text-gray-600'>Don't have an account yet? <br /> <Link to="/register" className='underline'>Sign Up</Link></p>
+            </div>
+          </form>
+        </div>    
       </div>
     </div>
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,33 +3,33 @@ import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div>
-        <div>
-            <h2>Create An Account</h2>
-        </div>
-        <div>
-            <form action="">
-                <div>
-                    <label htmlFor="">Username</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label htmlFor="">Email Address</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label htmlFor="">Passowrd</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <label htmlFor="">Confirm Passoword</label>
-                    <input type="text" />
-                </div>
-                <div>
-                    <button>Sign up</button>
-                    <p>Already have an account <Link to="/login">Login</Link></p>
-                </div>
-            </form>
+    <div className='min-h-screen flex justify-center items-center border-2 pt-12'>
+        <div className=''>
+            <div className=''>
+                <h2 className='text-2xl text-bold text-center text-gray-600'>Create An Account</h2>
+            </div>
+            <div className=' mt-5'>
+                <form action="" className='mb-20 gap-4 flex flex-col'>
+                    <div>
+                        <input type="text" placeholder='Full Name' className='border-3 px-[0.5rem] py[0.5rem] w-[21.42rem] h-[2.4rem] rounded-[0.3rem] border-gray-300'/>
+                    </div>
+                    <div>
+                        <input type="text" placeholder='Email' className='border-3 px-[0.5rem] py[0.5rem] h-[2.4rem] w-[21.42rem] rounded-[0.3rem] border-gray-300'/>
+                    </div>
+                    <div>
+                        <input type="text" placeholder='Password' className='border-3 px-[0.5rem] py[0.5rem] w-[21.42rem] h-[2.4rem] rounded-[0.3rem] border-gray-300'/>
+                    </div>
+                    <div>
+                        <input type="text" placeholder='Confirm Password' className='border-3 px-[0.5rem] py[0.5rem] w-[21.42rem] h-[2.4rem] rounded-[0.3rem] border-gray-300'/>
+                    </div>
+                    <div className='border-2 px-[8rem] py-[0.5rem] rounded-[0.3rem] border-gray-400 bg-gray-500 items-center text-white textbold text-2xl'>
+                        <button>Sign up</button>
+                    </div>
+                    <div>
+                        <p className='text-center text-gray-600'>Already have an account? <br /> <Link to="/login" className='underline'>Login</Link></p>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
   )
