f15036e8cf2bf46b65822871b6ced6b9859e8a3b
Ajibade Ridwan <ridwanabiola2000@gmail.com>
2025-05-06 09:41:15 +0100

    Updated Changes

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Sidebar = () => {
   return (
     <div className='flex flex-col gap-4 p-4 h-full primary shadow-md'>
+      <Link to="/register">Sign Up</Link>      
       <Link to="/login">Login</Link>
-      <Link to="/register">Sign Up</Link>
       {/* Add more links as needed */}
     </div>
   );
